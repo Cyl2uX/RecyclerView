9e81cca3a4c666776ff65705b8f2383a2c7a192b
Cyrux <cyl2ux@gmail.com>
2020-06-15 02:43:34 -0400

    Populate RecyclerView

--- app/src/main/java/mx/com/cyrux/recyclerview/MainActivity.java
+++ app/src/main/java/mx/com/cyrux/recyclerview/MainActivity.java
@@ -1,14 +1,22 @@
 package mx.com.cyrux.recyclerview;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView rvCars;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView rvCars = findViewById(R.id.rvCars);
+        rvCars.setHasFixedSize(true);
+        rvCars.setLayoutManager(new LinearLayoutManager(this));
+        rvCars.setAdapter(new CarAdapter(Car.getCarsList()));
     }
 }
